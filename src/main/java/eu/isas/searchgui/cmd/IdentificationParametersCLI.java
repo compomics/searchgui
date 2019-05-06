5a09e105d02d424c847daf34fe9f3448054bbb30
Harald Barsnes <harald.barsnes@gmail.com>
2019-05-06 16:00:05 +0200

    The paths are now updated before the IdentificationParametersCLI is executed. Moved project to version 3.3.14-beta.

--- src/main/java/eu/isas/searchgui/cmd/IdentificationParametersCLI.java
+++ src/main/java/eu/isas/searchgui/cmd/IdentificationParametersCLI.java
@@ -2,27 +2,42 @@ package eu.isas.searchgui.cmd;
 
 import com.compomics.software.CompomicsWrapper;
 import com.compomics.cli.identification_parameters.AbstractIdentificationParametersCli;
-import eu.isas.searchgui.SearchHandler;
-import java.io.File;
+import com.compomics.util.gui.waiting.waitinghandlers.WaitingHandlerCLIImpl;
+import com.compomics.util.waiting.WaitingHandler;
 import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 /**
  * The SearchParametersCLI allows creating search parameters files using command
  * line arguments.
  *
  * @author Marc Vaudel
+ * @author Harald Barsnes
  */
 public class IdentificationParametersCLI extends AbstractIdentificationParametersCli {
 
     /**
-     * Construct a new SearchParametersCLI runnable from a list of arguments.
+     * The waiting handler.
+     */
+    private WaitingHandler waitingHandler;
+    
+    /**
+     * Construct a new IdentificationParametersCLI runnable from a list of arguments.
      * When initialization is successful, calling "run" will write the created
      * parameters file.
      *
      * @param args the command line arguments
      */
     public IdentificationParametersCLI(String[] args) {
-        initiate(args);
+        try {
+            waitingHandler = new WaitingHandlerCLIImpl();
+            // check if there are updates to the paths
+            String[] nonPathSettingArgsAsList = PathSettingsCLI.extractAndUpdatePathOptions(args);
+            initiate(nonPathSettingArgsAsList);
+        } catch (ParseException ex) {
+            waitingHandler.appendReport("An error occurred while running the command line.", true, true);
+            ex.printStackTrace();
+        }
     }
 
     /**
